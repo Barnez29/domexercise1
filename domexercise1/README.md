d9cf82fe3bbf5ac442db10f1f0597f784a3527ca
Barnez29 <Barnaby.davids@gmail.com>
2020-03-17 19:06:36 +0200

    dom exercise use button to change paragraph color
    dom exercise use button to change paragraph color

---
+++ domexercise1/README.md
@@ -0,0 +1,2 @@
+# domexercise1
+DOM exercise 1 - Change text using button
---
+++ domexercise1/js/main.js
@@ -0,0 +1,7 @@
+function bluetext() {
+    document.getElementById("changetext").style.color = "blue";
+}
+
+function redtext() {
+    document.getElementById("changetext").style.color = "red";
+}
